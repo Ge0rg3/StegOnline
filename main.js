c80a7f58c9a5edd5ff0651a7a6a31ad4e0d97347
George O <16269580+Ge0rg3@users.noreply.github.com>
2019-02-06 19:05:48 +0000

    Core Functionality
    This provides the core functionality of the site, including the ability to browse through (non-transparent) image planes, and extract data from any bit plane.

---
+++ main.js
@@ -0,0 +1,201 @@
+var planeNo = 0;
+
+$(document).ready(function(){
+
+    //Close popup box on click
+    $('#close').click(function(e) {
+        $("#lsbExtractPopup").fadeOut();
+    });
+
+});
+
+
+function handleFileSelect(evt) {
+  /*
+  Live file reader management.
+  Loaded image object is sent to the run() function.
+  */
+  var f = evt.target.files[0];
+
+  if (!f.type.match('image.*')) {
+    alert("Please enter valid image file.");
+    return;
+  }
+
+  var reader = new FileReader();
+
+  reader.readAsDataURL(f);
+
+  reader.onloadend = function(){
+    let imageObj = new Image();
+    imageObj.onload = function() {
+      run(imageObj, this.width, this.height);
+    }
+    imageObj.src = reader.result;
+  }
+
+}
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+function run(imageObj, width, height) {
+  /*
+  Main control function
+  Input: Image object, image width, image height
+  If image has transparency, send to new page
+  If not, display image control features.
+  */
+  //Initialise canvas
+  canvas = document.createElement("canvas");
+  ctx = canvas.getContext("2d");
+  ctx.imageSmoothingEnabled = false;
+  canvas.width = width;
+  canvas.height = height;
+
+  //Get image data
+  ctx.drawImage(imageObj, 0, 0);
+  rgbaData = ctx.getImageData(0, 0, width, height).data;
+  r = rgbaData.filter((val, index) => index % 4 == 0);
+  g = rgbaData.filter((val, index) => (index-1) % 4 == 0);
+  b = rgbaData.filter((val, index) => (index-2) % 4 == 0);
+  a = rgbaData.filter((val, index) => (index-3) % 4 == 0);
+
+  $("#image").html("");
+
+  if (a.filter(_ => _ < 255).length > 0) {
+    $("#transparent").show();
+    return;
+  }
+
+  restore();
+  $("#transparent").hide();
+  $("#image").append(canvas);
+
+}
+
+function planes() {
+  /*
+    Uses the displayPlane helper function to show the planes.
+    Cycles through values in "transforms" const.
+  */
+  let transformKeys = Object.keys(transforms);
+  $("#currentPlane").html(transformKeys[planeNo]);
+
+  displayPlane(...transforms[transformKeys[planeNo]]);
+
+  if (++planeNo >= 32) planeNo = 0;
+
+}
+
+function initPlanes() {
+  $("#planesTestBtn").hide();
+  $("#rgbaPlanes").show();
+
+  let transformKeys = Object.keys(transforms);
+  $("#currentPlane").html(transformKeys[planeNo]);
+  displayPlane(...transforms[transformKeys[planeNo]]);
+}
+
+function browsePlanes(action, direction) {
+  /*
+    Used to traverse the RGBA plane viewer.
+    Input:
+      - action ('next' or 'skip'): Go to next plane, or skip to next colour
+      - direction ('forward' or 'backward'): Traverse forwards or backwards
+  */
+  if (planeNo >= 32) planeNo = 0;
+  if (planeNo < 0) planeNo = 31;
+  if (action == 'next') {
+    planeNo += (direction == 'forward' ? 1 : -1);
+  }
+  else if (action == 'skip') {
+    if (direction == 'forward') {
+      if (planeNo >= 24) planeNo = 0;
+      else if (planeNo >= 16) planeNo = 24;
+      else if (planeNo >= 8) planeNo = 16;
+      else if (planeNo >= 0) planeNo = 8;
+    }
+    else if (direction == 'backward') {
+      if (planeNo >= 24) planeNo = 16;
+      else if (planeNo >= 16) planeNo = 8;
+      else if (planeNo >= 8) planeNo = 0;
+      else if (planeNo >= 0) planeNo = 24;
+    }
+  }
+
+  let transformKeys = Object.keys(transforms);
+  $("#currentPlane").html(transformKeys[planeNo]);
+  displayPlane(...transforms[transformKeys[planeNo]]);
+}
+
+function parseTable() {
+  /*
+    TODO: Rework into JQuery
+    Parses the table on the page, to produce an array ready to be parsed into lsb() function
+    For more info on the lsb function, see rgbaFunctions.js
+
+  */
+  var table = document.getElementById("lsbtable");
+  let order = ['R','G','B','A'];
+  let lsbParams = [[],[], 0, 0];
+  let oldOrder = [];
+  $("#lsbError").html("");
+
+  //Bit planes
+  for (let i=0; i < order.length; i++) {
+    let selectedBits = [];
+    for (let j=1; j < table.rows.length; j++) {
+      var checked = table.rows.item(j).cells[i].childNodes[1].checked;
+      if (checked) selectedBits.push(8-j);
+    }
+    if (selectedBits.length > 0) {
+      oldOrder.push(order[i]);
+      lsbParams[1].push(selectedBits);
+    }
+  }
+  //Extra options
+  lsbParams[2] = $("#extractRow").is(":checked") ? 'row' : 'column';
+  lsbParams[3] = $("#orderMSB").is(":checked") ? 'msb' : 'lsb';
+    //Custom order
+  planeOrder = [$("#rgbaOne").val(),$("#rgbaTwo").val(),$("#rgbaThree").val(),$("#rgbaFour").val()];
+    //Error if duplicate, or if no entries
+  let sorted = planeOrder.sort();
+  if (sorted[0] != "A" || sorted[1] != "B" || sorted[2] != "G" || sorted[3] != "R") {
+    $("#lsbError").html("Error: Duplicate colours in Bit Plane Order.&nbsp;&nbsp;");
+    return;
+  }
+  if (oldOrder.length == 0) {
+    $("#lsbError").html("Error: No bit planes selected!&nbsp;&nbsp;&nbsp;");
+    return;
+  }
+  let newOrder = [];
+  for (colour of planeOrder) {
+    if (oldOrder.indexOf(colour) != -1) newOrder.push(colour);
+  }
+    //Replace letters with actual RGBA arrays
+  for (colour of newOrder) {
+    if (colour == "R") lsbParams[0].push(r);
+    if (colour == "G") lsbParams[0].push(g);
+    if (colour == "B") lsbParams[0].push(b);
+    if (colour == "A") lsbParams[0].push(a);
+  }
+  return lsbParams;
+}
+
+
+async function extractLsb() {
+  /*
+    Bridge between extracting table data, runnig lsb function and outputting hex into text box
+  */
+  let tableParams = parseTable();
+  let hex = await lsb(tableParams[0], tableParams[1], tableParams[2], tableParams[3], tableParams[4]);
+  let ascii = hexToAscii(hex);
+  //Split ascii into chunks
+  ascii = ascii.match(/.{1,8}/g).join(' ');
+  $("#asciioutput").val(ascii);
+  $("#hexoutput").val(hex);
+}
+
+
+function showExtractPopup() {
+  $("#lsbExtractPopup").fadeIn();
+}
---
+++ rgbaFunctions.js
@@ -0,0 +1,261 @@
+//Calculating columns within lsb() takes forever, so we only want to do it once
+var rC, gC, bC, aC, extracting=false;
+
+const transforms = {
+  "Red plane 0" : ['r',0], //0
+  "Red plane 1" : ['r',1], //1
+  "Red plane 2" : ['r',2], //2
+  "Red plane 3" : ['r',3], //3
+  "Red plane 4" : ['r',4], //4
+  "Red plane 5" : ['r',5], //5
+  "Red plane 6" : ['r',6], //6
+  "Red plane 7" : ['r',7], //7
+  "Green plane 0" : ['g',0], //8
+  "Green plane 1" : ['g',1], //9
+  "Green plane 2" : ['g',2],//10
+  "Green plane 3" : ['g',3],//11
+  "Green plane 4" : ['g',4],//12
+  "Green plane 5" : ['g',5],//13
+  "Green plane 6" : ['g',6],//14
+  "Green plane 7" : ['g',7],//15
+  "Blue plane 0" : ['b',0], //16
+  "Blue plane 1" : ['b',1], //17
+  "Blue plane 2" : ['b',2],  //18
+  "Blue plane 3" : ['b',3],  //19
+  "Blue plane 4" : ['b',4],  //20
+  "Blue plane 5" : ['b',5],  //21
+  "Blue plane 6" : ['b',6],  //22
+  "Blue plane 7" : ['b',7],  //23
+  "Alpha plane 0" : ['a',0], //24
+  "Alpha plane 1" : ['a',1], //25
+  "Alpha plane 2" : ['a',2], //26
+  "Alpha plane 3" : ['a',3], //27
+  "Alpha plane 4" : ['a',4], //28
+  "Alpha plane 5" : ['a',5], //29
+  "Alpha plane 6" : ['a',6], //30
+  "Alpha plane 7" : ['a',7] //31
+}
+
+//Sleep helper for async functions
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+function generateImage(r, g, b, a) {
+  /*
+    Input: 4 arrays containing all Red, Green, Blue and Alpha values respectively.
+    Displays created image object onto canvas
+  */
+  var combinedRGBA = [];
+  for(let i=0; i < r.length; i++) {
+    combinedRGBA.push(r[i], g[i], b[i], a[i]);
+  }
+  combinedRGBA = new Uint8ClampedArray(combinedRGBA);
+  let newImageObject = new ImageData(combinedRGBA, canvas.width, canvas.height);
+  ctx.putImageData(newImageObject, 0, 0);
+}
+
+
+function removeAlpha() {
+  /*
+    Uses global r, g, b & a constants.
+    Sets all values in Alpha channel to 255, thus making them opaque.
+    After changine alpha values, the image is redrawn.
+  */
+  a = new Array(a.length).fill(255);
+  restore();
+}
+
+
+function restore() {
+  /*
+    Uses global r, g, b & a constants.
+    Draws regular loaded image onto canvas, and resets RGBA planes
+  */
+  planeNo = 0;
+  generateImage(r, g, b, a);
+}
+
+
+function xor(isTransparent) {
+  /*
+    Uses global r, g, b & a constants.
+    Input: True/False, does the image have transparency.
+    Draws Xor'd image onto canvas.
+  */
+  restore();
+  let xorR = r.map(_ => _^255);
+  let xorG = g.map(_ => _^255);
+  let xorB = b.map(_ => _^255);
+  let xorA = isTransparent ? xorA = a.map(_ => _^255) : new Array(a.length).fill(255);
+  generateImage(xorR, xorG, xorB, xorA);
+}
+
+
+function full(colour) {
+  /*
+    Displays only one colour channel (with FULL alpha).
+    Input: 'r', 'g' 'b' or 'a'.
+    Draws onto canvas.
+    We display alpha by assigning it's value to RGB
+  */
+  restore();
+  let newR = colour == 'r' ? r : Array(r.length).fill(0);
+  let newG = colour == 'g' ? g : Array(g.length).fill(0);
+  let newB = colour == 'b' ? b : Array(b.length).fill(0);
+  if (colour == 'a') {
+    let tempA = a.map(_ => _^255);
+    newR = newG = newB = tempA;
+  };
+  let newA = Array(r.length).fill(255);
+  generateImage(newR, newG, newB, newA);
+}
+
+
+
+function displayPlane(colour, number) {
+  /*
+    Input: Colour ('r', 'g', 'b' or 'a') and a number (0-7)
+    This will draw the relevant colour plane onto the canvas.
+  */
+  let newR = [], newG = [], newB = [];
+  let newA = Array(r.length).fill(255);
+
+  let colSelection = ['r','g','b','a'].indexOf(colour);
+  let planeView = [r, g, b, a][colSelection];
+
+  for(let i=0; i < r.length; i++) {
+    //Store binary version of colour
+    let bin = intToBin(planeView[i]).split('');
+    //If the nth LSB in the binary string is 1, print black
+    if (bin[bin.length-1-number] == "1") {
+      newR.push(255);
+      newG.push(255);
+      newB.push(255);
+    }
+    else {
+      newR.push(0);
+      newG.push(0);
+      newB.push(0);
+    }
+  }
+
+  generateImage(newR, newG, newB, newA);
+}
+
+
+function rgbToHex(r, g, b) {
+    /*
+      Input: r, g and b values (a is defaulted to 255).
+      Output: Hex string
+      This was ripped off StackOverflow (https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb)
+    */
+    return "0x" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+function intToBin(n) {
+  /*
+    Input: n, an integer
+    Int -> Binary String
+    Output: Binary string (Minimum 8 charcaters)
+  */
+  let bin = (n >>> 0).toString(2)+"";
+  if (bin.length < 8) return ("0".repeat(8-bin.length))+bin;
+  return bin;
+}
+
+function hexToAscii(hex) {
+  /*
+    Input: hex, a hex string
+    Output: An ascii string, with any non printable characters displayed by a "."
+  */
+  hex = hex+"";
+  let ascii = "";
+  for (let i=0; i < hex.length; i+=2) {
+    let charCode = parseInt(hex.substr(i, 2), 16);
+    if (charCode > 126 || charCode < 32) ascii += ".";
+    else ascii += String.fromCharCode(charCode);
+  }
+  return ascii;
+}
+
+
+async function lsb(colours, bits, extractBy='row', bitOrder='msb') {
+  /*
+    TODO: Speed up columnar version
+
+    Returns the hex representation for the LSB of the image
+    Input:
+      -Array of colour arrays (e.g [r] / [r, g] / [b, g, a])
+        -Please note the order of the colours equates to the order of the LSB
+      -Array of sub-arrays containing the bits to extract
+      - (Optional) Extraction direction, either 'row' or 'column'
+    Output:
+      -Hex LSB data
+    Example uses:
+    let lsbHex = await lsb([r], [[5,4,3,2,1,0]]);
+      let lsbHex = await lsb([r, g], [[0], [0]]);
+      let lsbHex = await lsb([g,b,r], [[1,2],[1,2]])
+  */
+  //Only do one extraction at a time!
+  if (extracting) return;
+  else extracting = true;
+  //This part is very slow, and the algorithm can probably be improved
+  //We have to turn the flat Uint8ClampedArray from one long joined list of rows to one long joined list of columns
+  if (extractBy == 'column') {
+    for (let i=0; i < colours.length; i++) {
+      let colour = colours[i];
+      //Use cached version if available
+      let cont = false;
+      if (colour == r && rC != null) colours[i] = rC, cont = true;
+      if (colour == g && gC != null) colours[i] = gC, cont = true;
+      if (colour == b && bC != null) colours[i] = bC, cont = true;
+      if (colour == a && aC != null) colours[i] = aC, cont = true;
+      if (cont) continue;
+      //If not, go!
+      let tempArr = [];
+      for (let j=0; j < canvas.width; j++) {
+        //Show loading every 100 runs
+        if (j%100==0) $("#loadingColumn").text(`${i+1}/${colours.length} : ${j}/${canvas.width}`), await sleep(0);
+        let filtered = Array.from(colour.filter((val, index) => index % canvas.width == j));
+        tempArr = tempArr.concat(filtered);
+      }
+      $("#loadingColumn").text("Complete!");
+      colours[i] = new Uint8ClampedArray(tempArr);
+      //Create cached version
+      if (colour == r) rC = tempArr;
+      else if (colour == g) gC = tempArr;
+      else if (colour == b) bC = tempArr;
+      else if (colour == a) aC = tempArr;
+    }
+  }
+  //Start extraction
+  var bin = "";
+  var hex = "";
+  var bitCount = 1;
+  for (let i=0; i < r.length; i++) { //For each pixel
+    for (let j=0; j < colours.length; j++) { //For each inputted colour
+      //Convert rgb value to binary
+      let binaryCol = intToBin(colours[j][i]);
+      //Get last n bits of binary
+      let binSub = binaryCol.split('').filter((val, index) => bits[j].indexOf(7-index) != -1);
+      if (bitOrder == 'lsb') binSub = binSub.reverse();
+
+      for (char of binSub) {
+        bin += char;
+        if (bitCount % 8 == 0) { //If one byte, turn to hex & add to hex string
+          let tempHex = parseInt(bin, 2).toString(16);
+          if (tempHex.length == 1) hex += ("0"+tempHex);
+          else hex += tempHex;
+          bin = "";
+        }
+        bitCount++;
+      }
+
+    }
+  }
+  //Clean up remaining bits
+  if (bin.length > 0) {
+    hex += parseInt(bin, 2).toString(16);
+  }
+  extracting = false;
+  return hex;
+}
